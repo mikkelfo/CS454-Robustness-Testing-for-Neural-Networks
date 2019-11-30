6233c1fb531468f3426cbf1f8c4abb02e59670b6
Thomas Mattsson <tmattsson0@gmail.com>
2019-11-30 11:29:19 +0900

    More scafolding for mutate function

--- GA.py
+++ GA.py
@@ -9,12 +9,14 @@
 import numpy as np
 import random
 
+
 class mask:
     def __init__(self, shapes=[]):
         self.shapes = shapes
         self.change = 0
         self.accuracy = 0
-        
+
+
     def getMaskChange(self):
         value = 0
         for i in self.shapes:
@@ -29,19 +31,22 @@ class shape:
         self.points = points
         self.RGBValues = RGBValues
         self.change = 0
-        
+
+
     #This needs to be replaced or we need to sort the points
     def polygonArea(self):
         x = self.points[0::2]
         y = self.points[1::2]
         return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
 
+
     def getShapeChange(self):
         rgbChange = 0
         for i in self.RGBValues:
             rgbChange += abs(i)
-        self.change= self.polygonArea() + rgbChange
+        self.change = self.polygonArea() + rgbChange
         return self.change
+
 
 def getRandomPoints(size, maxPoints):
     if size % 2 != 0:
@@ -54,13 +59,15 @@ def getRandomPoints(size, maxPoints):
         points.append(x)
         points.append(y)
     return points
-    
+
+
 def getRandomRGBValues():
     rgb = []
     rgb.append(random.uniform(-255.0,255.0))
     rgb.append(random.uniform(-255.0,255.0))
     rgb.append(random.uniform(-255.0,255.0))
     return rgb
+
 
 def initPopulation(popSize, maxShapes, shapeSize, maxPoints, imageSize):
     population = []
@@ -83,6 +90,7 @@ def initPopulation(popSize, maxShapes, shapeSize, maxPoints, imageSize):
 
     return population
 
+
 def crossover(parent1, parent2):
     nr1 = random.randint(1,len(parent1.shapes))
     nr2 = random.randint(1,len(parent2.shapes))
@@ -97,32 +105,59 @@ def crossover(parent1, parent2):
     child = mask(child)
     return child
 
-def mutation(mutationrate):
-    changeShape = False
-    changeCenpoint = False
-    changeRGB = False
+
+def createRandomPoint(self, diff=10):
+    xc, yc = self.centerPoint
+    xr, yr = [xc - diff, xc + diff], [yc - diff, yc + diff]
+
+    x, y = random.choice(range(xr[0], xr[1])), random.choice(range(yr[0], yr[1]))
+
+    if x not in range(0, self.dim) or y not in range(0, self.dim):
+        return self.createRandomPoint(diff)
+
+    return x, y
+
+
+def mutate_rgb_values():
+    pass
+
+
+def mutation(shape_mut):
+    change_shape = False
+    change_cenpoint = False
+    change_rgb = False
 
     # Decide how many things will be mutated
-    numOfMut = random.randint(1, 3)
+    num_of_mut = random.randint(1, 3)
 
     num_set = {1, 2, 3}
 
     # Pull out the things that will be mutated and set their value to 'True'
-    choice_set = random.sample(num_set, numOfMut)
+    choice_set = random.sample(num_set, num_of_mut)
 
     if 1 in choice_set:
-        changeShape = True
+        change_shape = True
     if 2 in choice_set:
-        changeCenpoint = True
+        change_cenpoint = True
     if 3 in choice_set:
-        changeRGB = True
+        change_rgb = True
 
+    # if change_shape:
+        #code
 
+    if change_cenpoint:
+        shape_mut.center = createRandomPoint(shape_mut.center)
+
+    # if change_rgb:
+    #     #code
+    
+    return shape_mut
 
 #   we can mutate center position, shape and rgb values. one or all three can be mutated.
 #   centerpoint is mutated by randomising a new point
 #   shape is mutated by either removing or adding x points (and moving?)
 #   rgb values can either decrease or increase with value y. all or only one value can be changed. (roll to see how many values, then again to see which values)
+
 
 populationSize = 10
 maxShapes = 20
@@ -130,9 +165,11 @@ shapeSize = 25
 maxPoints = 8
 imageSize = 256
 
-# mutation(2)
 
-pop = initPopulation(populationSize, maxShapes, shapeSize, maxPoints, imageSize)
-for i in range(0,len(pop)):
-     print("Change of the mask: %8.3f Number of shapes in the mask: %d" %(pop[i].change, len(pop[i].shapes)))
+mutation()
+
+
+# pop = initPopulation(populationSize, maxShapes, shapeSize, maxPoints, imageSize)
+# for i in range(0,len(pop)):
+#      print("Change of the mask: %8.3f Number of shapes in the mask: %d" %(pop[i].change, len(pop[i].shapes)))
 
