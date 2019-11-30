b80b2e38a30544dfb73c65596a0fef5bc105fe00
Kyu-Sang Kim(DESKTOP) <kyu-sang.kim@student.unsw.edu.au>
2019-11-30 12:10:03 +0900

    works unless delauney/init_points exception calls (unreliable)

--- GA.py
+++ GA.py
@@ -6,61 +6,10 @@
 # shape should have the amount of changed pixels and rgb change, mask should have a sum of all shapes
 # keep the points relative
 
-import numpy as np
 import random
 
-class mask:
-    def __init__(self, shapes=[]):
-        self.shapes = shapes
-        self.change = 0
-        self.accuracy = 0
-        
-    def getMaskChange(self):
-        value = 0
-        for i in self.shapes:
-            value += i.change
-        self.change = value
-        return self.change
-    
-    
-class shape:
-    def __init__(self, center=[], points=[], RGBValues=[]):
-        self.center = center
-        self.points = points
-        self.RGBValues = RGBValues
-        self.change = 0
-        
-    #This needs to be replaced or we need to sort the points
-    def polygonArea(self):
-        x = self.points[0::2]
-        y = self.points[1::2]
-        return 0.5*np.abs(np.dot(x,np.roll(y,1))-np.dot(y,np.roll(x,1)))
+from mask import *
 
-    def getShapeChange(self):
-        rgbChange = 0
-        for i in self.RGBValues:
-            rgbChange += abs(i)
-        self.change= self.polygonArea() + rgbChange
-        return self.change
-
-def getRandomPoints(size, maxPoints):
-    if size % 2 != 0:
-        size -= 1
-    nrOfPoints = random.randint(3,maxPoints)
-    points = []
-    for i in range(0, nrOfPoints):
-        x = random.randint(-size/2,(size/2))
-        y = random.randint(-size/2,(size/2))        
-        points.append(x)
-        points.append(y)
-    return points
-    
-def getRandomRGBValues():
-    rgb = []
-    rgb.append(random.uniform(-255.0,255.0))
-    rgb.append(random.uniform(-255.0,255.0))
-    rgb.append(random.uniform(-255.0,255.0))
-    return rgb
 
 def initPopulation(popSize, maxShapes, shapeSize, maxPoints, imageSize):
     population = []
@@ -68,25 +17,19 @@ def initPopulation(popSize, maxShapes, shapeSize, maxPoints, imageSize):
         nrOfShapes = random.randint(1, maxShapes)
         shapes = []
         for j in range(0, nrOfShapes):
-            points = getRandomPoints(shapeSize, maxPoints)
-            rgb = getRandomRGBValues()
-            center = []
-            center.append(random.randint(0,imageSize))
-            center.append(random.randint(0,imageSize))
-            tempShape = shape(center, points, rgb)
-            tempShape.getShapeChange()
-            shapes.append(tempShape)
-    
-        tempMask = mask(shapes)
-        tempMask.getMaskChange()
-        population.append(tempMask)
+            # change to passing random amount of points 3 to max
+            shapes.append(Shape(random.randint(3, maxPoints)))
+
+        # get the shapes into mask using new function
+        population.append(Mask(shapes))
 
     return population
 
+
 def crossover(parent1, parent2):
-    nr1 = random.randint(1,len(parent1.shapes))
-    nr2 = random.randint(1,len(parent2.shapes))
-    
+    nr1 = random.randint(1, len(parent1.shapes))
+    nr2 = random.randint(1, len(parent2.shapes))
+
     child = []
     temp1 = random.sample(parent1.shapes, nr1)
     for i in temp1:
@@ -94,8 +37,10 @@ def crossover(parent1, parent2):
     temp2 = random.sample(parent2.shapes, nr2)
     for i in temp2:
         child.append(i)
-    child = mask(child)
-    return child
+
+    # get children shapes into mask
+    return Mask(child)
+
 
 def mutation(mutationrate):
     changeShape = False
@@ -118,11 +63,11 @@ def mutation(mutationrate):
         changeRGB = True
 
 
-
 #   we can mutate center position, shape and rgb values. one or all three can be mutated.
 #   centerpoint is mutated by randomising a new point
 #   shape is mutated by either removing or adding x points (and moving?)
 #   rgb values can either decrease or increase with value y. all or only one value can be changed. (roll to see how many values, then again to see which values)
+
 
 populationSize = 10
 maxShapes = 20
@@ -132,7 +77,18 @@ imageSize = 256
 
 # mutation(2)
 
-pop = initPopulation(populationSize, maxShapes, shapeSize, maxPoints, imageSize)
-for i in range(0,len(pop)):
-     print("Change of the mask: %8.3f Number of shapes in the mask: %d" %(pop[i].change, len(pop[i].shapes)))
+pop = initPopulation(populationSize, maxShapes,
+                     shapeSize, maxPoints, imageSize)
 
+for i in range(0, len(pop)):
+    print("Change of the mask: %8.3f Number of shapes in the mask: %d" %
+          (pop[i].getMaskChange(), len(pop[i].shapes)))
+
+#new_pop = []
+#for i in pop:
+#    selection = random.sample(pop, 2)
+#    new_pop.append(crossover(selection[0], selection[1]))
+
+#for i in range(0, len(new_pop)):
+#    print("Change of the mask: %8.3f Number of shapes in the mask: %d" %
+#          (new_pop[i].getMaskChange(), len(new_pop[i].shapes)))
