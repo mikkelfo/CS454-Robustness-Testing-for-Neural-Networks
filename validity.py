f2fac5e9c3653991e1e7ddca32789213c6c1c687
mikkelfo <mikk528a@gmail.com>
2019-12-11 01:20:57 +0900

    Added validity file that can Check for point validity (within range) Check for list validity (points within range) Check for list validity AND valid Delauney

---
+++ validity.py
@@ -0,0 +1,26 @@
+from scipy.spatial import Delaunay
+
+def validate(listOfPoints):
+    if validate_list(listOfPoints):
+        try:
+            Delaunay(listOfPoints)
+            return True
+        except:
+            return False
+    else:
+        return False
+
+
+def validate_list(listOfPoints):
+    for point in listOfPoints:
+        if not validate_point(point):
+            return False
+    return True
+
+
+def validate_point(point, dim=299):
+    x, y = point
+    if x in range(0, dim) and y in range(0, dim):
+        return True
+    else:
+        return False
