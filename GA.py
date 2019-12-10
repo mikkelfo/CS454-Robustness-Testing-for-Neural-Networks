d5d350daf15f8824744adea28d8848de4e687025
Martin <humin.m@gmail.com>
2019-12-10 20:57:59 +0900

    removed old 'run' in GA

--- GA.py
+++ GA.py
@@ -109,32 +109,3 @@ def legalRGBValue(RGBValue, RGBRange):
         RGBValue += random.randint(-rDiffFromMin, (RGBRange / 2) + ((RGBRange/2) - rDiffFromMin))
 
     return RGBValue
-
-
-populationSize = 10
-maxShapes = 20
-shapeSize = 25
-maxPoints = 8
-imageSize = 256
-mutationRate = 0.02
-
-pop = initPopulation(populationSize, maxShapes,
-                     shapeSize, maxPoints, imageSize)
-
-for i in range(0, len(pop)):
-    print("Change of the mask: %8.3f Number of shapes in the mask: %d" %
-          (pop[i].getMaskChange(), len(pop[i].shapes)))
-
-new_pop = []
-for i in pop:
-    selection = random.sample(pop, 2)
-    childMask = crossover(selection[0], selection[1])
-    for shape in childMask.shapes:
-        if random.random() < mutationRate:
-            mutation(shape)
-
-    new_pop.append(childMask)
-
-for i in range(0, len(new_pop)):
-    print("Change of the mask: %8.3f Number of shapes in the mask: %d" %
-          (new_pop[i].getMaskChange(), len(new_pop[i].shapes)))
