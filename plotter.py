f1534502a0e1dd68b045379cca52c2269a484770
Kyu-Sang Kim(DESKTOP) <kyu-sang.kim@student.unsw.edu.au>
2019-12-16 12:27:52 +0900

    plotter code revamped

--- plotter.py
+++ plotter.py
@@ -2,15 +2,21 @@ import matplotlib.pyplot as plt
 import pickle
 from mask import Mask
 
-read = open('pareto_pop', 'rb')
-population = pickle.load(read)
-read.close()
+
+def load_pop():
+    with open('pareto_pop', 'rb') as read:
+        while True:
+            try:
+                yield pickle.load(read)
+            except EOFError:
+                break
+
 
 xAccuracy = []
 yChange = []
-for ind in population:
-	xAccuracy.append(ind.accuracy)
-	yChange.append(ind.change)
+for ind in load_pop():
+    xAccuracy.append(ind.accuracy)
+    yChange.append(ind.change)
 
 plt.scatter(xAccuracy, yChange, label="mask", color="black", marker="*", s=30)
 plt.xlabel("Accuracy")
@@ -18,4 +24,8 @@ plt.ylable("Change")
 plt.title("Pareto Population - 100, 40, 9")
 plt.legend()
 
-plt.show
+plt.show()
+
+dumpLog = open('dump_log', 'wb')
+pickle.dump([xAccuracy, yChange], dumpLog, -1)
+dumpLog.close()
