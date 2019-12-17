c5bf85f6a961e6761e8259554d71d80056fbcd3b
ChristPi <fried.kullmann@gmx.de>
2019-12-17 13:33:44 +0900

    cleanup analysis

--- analyse_mask.py
+++ analyse_mask.py
@@ -6,7 +6,7 @@ import edit_images as editor
 import copy
 import numpy as np
 
-read = open('pareto_pop3040', 'rb')
+read = open('pareto_pop', 'rb')
 population = pickle.load(read)
 read.close()
 
@@ -14,7 +14,6 @@ inception = fitnessfunction.load_model()
 original_images = fitnessfunction.get_images()
 labels = fitnessfunction.get_labels()
 
-img_number = 582
 number_of_images = 1000
 mask_analysis = 18
 
@@ -22,7 +21,6 @@ processed_images = np.zeros((number_of_images, 299, 299, 3))
 for i in range(number_of_images):
     processed_image = fitnessfunction.preprocess_for_eval(original_images[i])
     processed_images[i] = processed_image
-
 classes = inception.predict(processed_images)
 class_numbers = np.argmax(classes, 1)
 
